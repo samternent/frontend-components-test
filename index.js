6b9da35f1ebd554b2327bd2161cbed4555c437b8
Sam Ternent <sam.ternent@gmail.com>
2020-08-04 19:50:10 +0100

    fix bundling

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+export { default as TheHeader } from './src/components/layout/TheHeader.vue';
+export { default as TheLayout } from './src/components/layout/TheLayout.vue';
+export { default as TheSidebar } from './src/components/layout/TheSidebar.vue';
+export { default as PageNavigationTabs } from './src/components/navigation/PageNavigationTabs.vue';
+
 import vuetify from './src/plugins/vuetify';
-
-export const TheHeader = () =>
-  import('./src/components/layout/TheHeader.vue');
-
-export const TheLayout = () =>
-  import('./src/components/layout/TheLayout.vue');
-
-export const PageNavigationTabs = () =>
-  import('./src/components/navigation/PageNavigationTabs.vue');
 
 export const teamwork = {
   vuetify,
