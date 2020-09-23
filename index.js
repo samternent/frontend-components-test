275129b4f44c9368866a1c34fcbedebf55b5dddb
Sam Ternent <sam.ternent@gmail.com>
2020-09-23 23:26:23 +0100

    data table

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ export { default as TheLayout } from './src/components/layout/TheLayout.vue';
 export { default as TheSidebar } from './src/components/layout/TheSidebar.vue';
 export { default as TPageNavigationTabs } from './src/components/navigation/PageNavigationTabs.vue';
 
+export { VDataTable } from 'vuetify/lib';
+
 
 
 export { default as TButton } from './src/components/form/Button.vue';
